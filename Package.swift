846c965c370ace1e2ae12bcafd8c9c2f53389aae
Mistico <44604778+Mistico06@users.noreply.github.com>
2025-08-15 16:48:58 +0530

    fixed deps

--- Package.swift
+++ Package.swift
@@ -17,10 +17,10 @@ let package = Package(
             name: "MLCEngineObjC",
             path: "ios/MLCSwift/Sources/ObjC",
             cxxSettings: [
-                .headerSearchPath("../../tvm_home/include"),
-                .headerSearchPath("../../tvm_home/ffi/include"),
-                .headerSearchPath("../../tvm_home/3rdparty/dmlc-core/include"),
-                .headerSearchPath("../../tvm_home/3rdparty/dlpack/include")
+                .headerSearchPath("../../../../3rdparty/tvm/include"),
+                .headerSearchPath("../../../../3rdparty/tvm/ffi/include"),
+                .headerSearchPath("../../../../3rdparty/tvm/3rdparty/dmlc-core/include"),
+                .headerSearchPath("../../../../3rdparty/tvm/3rdparty/dlpack/include"),
             ]
         ),
         .target(
