5f38b2ef6916c1f3800d5921b89dc8fad319eea5
Mistico <44604778+Mistico06@users.noreply.github.com>
2025-08-15 14:43:43 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,36 +2,26 @@
 import PackageDescription
 
 let package = Package(
-    /* The name by which other projects will reference this package */
     name: "mlc-llm",
-    /* Supported platforms (match those used inside ios/MLCSwift) */
     platforms: [
         .iOS(.v15)
     ],
-    /* What this package exports to clients */
     products: [
         .library(
             name: "MLCSwift",
             targets: ["MLCEngineObjC", "MLCSwift"]
         )
     ],
-    /* External dependencies for MLCSwift itself (none today) */
-    dependencies: [
-        // When MLCSwift starts depending on external SPM packages,
-        // list them here.
-    ],
-    /* Tie the root package to the real sources in ios/MLCSwift */
+    dependencies: [],
     targets: [
         .target(
-            /* MLCEngineObjC target, ObjC++ sources */
             name: "MLCEngineObjC",
             path: "ios/MLCSwift/Sources/ObjC",
             publicHeadersPath: "include",
             cxxSettings: [
-                .headerSearchPath("../../tvm_home/include"),
-                .headerSearchPath("../../tvm_home/ffi/include"),
-                .headerSearchPath("../../tvm_home/3rdparty/dmlc-core/include"),
-                .headerSearchPath("../../tvm_home/3rdparty/dlpack/include"),
+                .headerSearchPath("../../../../3rdparty/tvm/include"),
+                .headerSearchPath("../../../../3rdparty/tvm/3rdparty/dmlc-core/include"),
+                .headerSearchPath("../../../../3rdparty/tvm/3rdparty/dlpack/include"),
                 .unsafeFlags(["-std=c++17"])
             ],
             linkerSettings: [
@@ -49,7 +39,6 @@ let package = Package(
             ]
         ),
         .target(
-            /* MLCSwift target, depends on MLCEngineObjC, Swift sources */
             name: "MLCSwift",
             dependencies: ["MLCEngineObjC"],
             path: "ios/MLCSwift/Sources/Swift"
