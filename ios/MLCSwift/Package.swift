8401d7252c2cf739531005bb08caec1392859a4b
Mistico <44604778+Mistico06@users.noreply.github.com>
2025-08-16 23:21:03 +0530

    Update Package.swift

--- ios/MLCSwift/Package.swift
+++ ios/MLCSwift/Package.swift
@@ -1,4 +1,6 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
@@ -14,31 +16,12 @@ let package = Package(
         .target(
             name: "MLCEngineObjC",
             path: "ios/MLCSwift/Sources/ObjC",
-            publicHeadersPath: "include",
-            cSettings: [
-                .define("DMLC_USE_LOGGING_LIBRARY", to: "1"),
-                .define("TVM_USE_LIBBACKTRACE", to: "0")
-            ],
             cxxSettings: [
                 .define("TVM_ALWAYS_INLINE", to: "__attribute__((always_inline)) inline"),
                 .headerSearchPath("../../../../3rdparty/tvm/include"),
                 .headerSearchPath("../../../../3rdparty/tvm/ffi/include"),
                 .headerSearchPath("../../../../3rdparty/tvm/3rdparty/dmlc-core/include"),
                 .headerSearchPath("../../../../3rdparty/tvm/3rdparty/dlpack/include"),
-                .unsafeFlags(["-std=c++17"])
-            ],
-            linkerSettings: [
-                .unsafeFlags([
-                    "-L" + Package.rootPath + "/ios/MLCSwift/lib",
-                    "-Wl,-all_load",
-                    "-lmodel_iphone",
-                    "-lmlc_llm",
-                    "-ltvm_runtime",
-                    "-ltokenizers_cpp",
-                    "-lsentencepiece",
-                    "-ltokenizers_c",
-                    "-Wl,-noall_load"
-                ])
             ]
         ),
         .target(
@@ -46,11 +29,6 @@ let package = Package(
             dependencies: ["MLCEngineObjC"],
             path: "ios/MLCSwift/Sources/Swift"
         )
-    ]
+    ],
+    cxxLanguageStandard: .cxx17
 )
-
-#if swift(>=5.7)
-extension Package {
-    static var rootPath: String { #filePath.split(separator: "/").dropLast().joined(separator: "/") }
-}
-#endif
